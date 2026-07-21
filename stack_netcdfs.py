598860bbd154dd2af9b7abe10943f517c0092878
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2026-07-21 19:57:32 -0400

    test

--- stack_netcdfs.py
+++ stack_netcdfs.py
@@ -16,6 +16,7 @@ cmbreen@discover32:/discover/nobackup/cmbreen/datacenters> sbatch sbatch_percent
 Submitted batch job 57253580
 
 '''
+import dask.array as da
 import xarray as xr
 import glob
 import xarray as xr
@@ -62,13 +63,24 @@ def process_region(raw_files_glob, thresh_file, out_file, region_name):
     # xarray 'time.season' outputs 'DJF', 'MAM', 'JJA', 'SON'
     ds['season'] = ds['time.season']
     
+        # Use dask to create an empty array of the right shape and chunks
+    empty_data = da.full(
+        shape, 
+        np.nan, 
+        chunks=(len(ds_thresh.quantile), 100, len(ds.lat), len(ds.lon))
+    )
     # 4. Build a continuous time-series of the correct seasonal thresholds
     # We create an empty array shaped like our data, then fill it season by season
     seasonal_thresholds = xr.DataArray(
-        np.nan, 
-        coords=[ds_thresh.quantile, ds.time, ds.lat, ds.lon], 
+        empty_data, 
+        coords={
+            'quantile': ds_thresh.quantile, 
+            'time': ds.time, 
+            'lat': ds.lat, 
+            'lon': ds.lon
+        },
         dims=['quantile', 'time', 'lat', 'lon']
-    ).chunk({'time': 100})
+    ).chunk({'time': 100}) ## chunk in 100 timesteps at a time 
     
     season_map = {
         'DJF': 'enthalpy_winter',
